00fcb633e950d22ae849b2b3edea3f978e411281
unknown <Yupimaperson101@protonmail.com>
2020-09-05 18:33:23 -0500

    Messed around with scripts, gravity , and collisions!

---
+++ CSCI 467 First game/Assets/Player_1i.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Player_1i : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+   
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello people who watch my snapchat stories");
+
+        rb.useGravity = false;
+      
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+
+    }
+}
