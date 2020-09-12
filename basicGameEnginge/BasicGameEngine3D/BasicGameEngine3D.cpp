96ff06fcf4f96d95fc11874973e5bc1b92ee31d8
unknown <Yupimaperson101@protonmail.com>
2020-09-12 00:28:32 -0500

    Finish working on Projection Matrix

--- basicGameEnginge/BasicGameEngine3D/BasicGameEngine3D.cpp
+++ basicGameEnginge/BasicGameEngine3D/BasicGameEngine3D.cpp
@@ -3,6 +3,38 @@
 
 #include "olcConsoleGameEngine.h"
 
+
+struct vec3d
+{
+    //Coordinates(points) in a 3D space 
+    float x, y, z;
+
+};
+
+
+struct triangle
+{
+    //Triangles resemble 3 vertices on the x,y,z planes
+    vec3d tri[3];
+};
+
+
+struct mesh
+{
+    //A fancy array of these triangles 
+    std::vector<triangle> meshgroup;
+
+};
+
+struct matrix
+{
+
+
+};
+
+
+
+
 class olcEngine3D: public olcConsoleGameEngine
 {
 
@@ -11,20 +43,64 @@ public:
 
     olcEngine3D()
     {
-    
+        //Enable unicode??
+        m_sAppName = L"3D Demo";
        
     }
 
+private: 
+    mesh meshcube;
 
 public:
     bool OnUserCreate() override
     {
-    
+        meshcube.meshgroup =
+        {
+
+        // 6 faces total of a cube
+        // SOUTH 2 triangles , 1 face, 3 points per verticie , 3 verticies per triangle
+        // 3  x  2 = 6 (x,y,z) points of the south face of the cube
+        { 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+        { 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+        // EAST                                                      
+        { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+        { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+
+        // NORTH                                                     
+        { 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+        { 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+
+        // WEST                                                      
+        { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+        { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+
+        // TOP                                                       
+        { 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+        { 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+        // BOTTOM                                                    
+        { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+        { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+        };
+
         //Continue running 
         return true;
     }
     bool OnUserUpdate(float fElapsedTime) override
     {
+
+        //Will clear the screen 
+        Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+        // Draw the triangle ( through projection ) 
+        for (auto tri : meshcube.meshgroup)
+        {
+
+        }
+
+       
 
         return true;
     }
